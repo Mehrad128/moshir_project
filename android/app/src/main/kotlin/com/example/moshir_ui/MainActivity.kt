4e3cf8fc72c2787df8335f7b0c328c77ba9bf903
MEHRAD_128 <hosseinmehraban83@gmail.com>
2026-02-19 02:41:42 +0330

    Reset android folder to default

--- android/app/src/main/kotlin/com/example/moshir_ui/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/moshir_ui/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.example.moshir_ui
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterActivity() {
-        override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
-}
+class MainActivity : FlutterActivity()
