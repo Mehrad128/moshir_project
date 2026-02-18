54403b4e887f4284baf694085883f625845aa58d
MEHRAD_128 <hosseinmehraban83@gmail.com>
2026-02-19 01:23:26 +0330

    Added All files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.android.tools.build:gradle:8.7.0") // آپدیت شده
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21") // آپدیت شده
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
