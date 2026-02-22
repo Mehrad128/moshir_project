b99c2bb33318823422b390101f3f72870369057f
MEHRAD_128 <hosseinmehraban83@gmail.com>
2026-02-22 23:53:33 +0330

    Added All files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext {
-        kotlin_version = '1.9.0'
-    }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
